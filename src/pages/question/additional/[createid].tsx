05cee41efe7790f962f788252133445f92d16585
João Paulo <devjoaopaulo2003@gmail.com>
2023-06-11 20:38:20 -0300

    feat: adicionando campo de select para a alternativa 5

--- src/pages/question/additional/[createid].tsx
+++ src/pages/question/additional/[createid].tsx
@@ -139,7 +139,7 @@ export default function createAdditionalQuestion() {
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
+          <Flex mt="8" justify="flex-start">
             <HStack spacing="4">
               <Link href="/question" passHref>
                 <Button as="a" colorScheme="red">Cancelar</Button>
